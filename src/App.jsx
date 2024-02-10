4cf7c7c0a41bad56367ef805a2dbda696f933b86
Manuel Heredia <spectrasonic117@gmail.com>
2024-02-10 16:45:11 -0600

    menu responsive with react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Cabecera from "./components/Cabecera";
+
+const App = () => {
+	return (
+		<div className='App'>
+			<Cabecera />
+		</div>
+	);
+};
+
+export default App;
